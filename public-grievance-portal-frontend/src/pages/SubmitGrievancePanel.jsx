601720faa0b4d73a0e0c4f3d7fbe3aa0d219abd2
mani-chauhan2004 <chauhan.mani1070@gmail.com>
2025-05-06 00:34:24 +0530

    Add Submit Grievance Page

---
+++ public-grievance-portal-frontend/src/pages/SubmitGrievancePanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GrievanceGatewayTopbar from '../components/GrievanceGatewayTopbar'
+import GrievanceForm from '../components/GrievanceForm'
+
+function SubmitGrievancePanel() {
+    return (
+        <div>
+            <GrievanceGatewayTopbar/>
+            <GrievanceForm/>
+        </div>
+    )
+}
+
+export default SubmitGrievancePanel
