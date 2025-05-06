a92cf86d2af13a9f6228b05dfa1ea0e76aab5074
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2025-05-06 13:39:20 +0530

    Add login and signup page

--- public-grievance-portal-frontend/src/App.jsx
+++ public-grievance-portal-frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import AdminDashboard from './pages/AdminDashboard'
 import SubmitGrievancePanel from './pages/SubmitGrievancePanel'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
   return (
-    <SubmitGrievancePanel/>
+    <>
+    <LoginPage/>
+    <SignupPage />
+    </>
+    // <SubmitGrievancePanel/>
   )
 }
 
